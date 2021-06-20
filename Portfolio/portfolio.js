53190dc69882b26e57208660b6feab16498dfd28
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-06-20 06:55:19 -0700

    added image to html, fixed display function in js

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -22,6 +22,10 @@ let triviaGameEl= document.getElementById("triviaGame");
 
 let portfolioEl= document.getElementById("portfolio");
 
+let portfoliov2El= document.getElementById("portfoliov2");
+
+//presenting all projects
+
 allEl.addEventListener('click', ()=>{
     makeMyDayAppEl.style.display= "";
     weatherDashboardEl.style.display= "";
@@ -30,9 +34,13 @@ allEl.addEventListener('click', ()=>{
     passwordGeneratorEl.style.display= "";
     dayPlannerEl.style.display= "";
     portfolioEl.style.display= "";
+    portfoliov2El.style.display= "";
 })
 
+//presenting all projects with HTML and CSS
+
 proj1El.addEventListener('click', ()=>{
+    //Display all 
     makeMyDayAppEl.style.display= "";
     weatherDashboardEl.style.display= "";
     triviaGameEl.style.display= "";
@@ -40,21 +48,48 @@ proj1El.addEventListener('click', ()=>{
     passwordGeneratorEl.style.display= "";
     dayPlannerEl.style.display= "";
     portfolioEl.style.display= "";
+    portfoliov2El.style.display= "";
 })
 
+//presenting all projects with JS and JQuery
+
 proj2El.addEventListener('click', ()=>{
+    //display
+    makeMyDayAppEl.style.display= "";
+    weatherDashboardEl.style.display= "";
+    triviaGameEl.style.display= "";
+    passwordGeneratorEl.style.display= "";
+    dayPlannerEl.style.display= "";
+    portfoliov2El.style.display= "";
+    //hide
     portfolioEl.style.display= "none";
 })
 
+//Presenting all projects with API, AJAX,and JSON 
+
 proj4El.addEventListener('click', ()=>{
+    //display
+    makeMyDayAppEl.style.display= "";
+    weatherDashboardEl.style.display= "";
+    dayPlannerEl.style.display= "";
+
+    //hide
     triviaGame.style.display= "none";
     passwordGeneratorEl.style.display= "none";
     portfolioEl.style.display= "none";
+    portfoliov2El.style.display= "none";
 })
 
+//presenting all projects using frameworks
 proj5El.addEventListener('click', ()=>{
+    //display
+    makeMyDayAppEl.style.display= "";
+    weatherDashboardEl.style.display= "";
+    portfoliov2El.style.display= "";
+
+    //hide
     portfolioEl.style.display= "none";
     dayPlannerEl.style.display= "none";
     triviaGameEl.style.display= "none";
-    passwordGeneratorEl.style.display= "none"
+    passwordGeneratorEl.style.display= "none";
 })
